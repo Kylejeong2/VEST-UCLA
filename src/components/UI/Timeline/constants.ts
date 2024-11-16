ea14cb4f62f57d06cce1219a414e1a7162372acc
Kylejeong2 <Kylejeong21@gmail.com>
2024-11-15 22:42:26 -0800

    copy changes'

---
+++ src/components/UI/Timeline/constants.ts
@@ -0,0 +1,10 @@
+export const desktopHeaderPhrase = ['Application Timeline'];
+export const desktopParagraphPhrase = [
+  'Our application process is designed to be smooth and easy; for the right candidates.',
+];
+
+// For mobile
+export const mobileHeaderPhrase = ['Application Timeline'];
+export const mobileParagraphPhrase = [
+  'Our application process is designed to be smooth and easy; for the right candidates.',
+];
