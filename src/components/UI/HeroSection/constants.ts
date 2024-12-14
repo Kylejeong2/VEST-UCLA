911c2b729eb029bf71a9ee94bdb1c635e7769e3e
Anish Kumar <anish@mac.lan>
2024-12-13 16:10:16 -0800

    changed title page's name to VEST

--- src/components/UI/HeroSection/constants.ts
+++ src/components/UI/HeroSection/constants.ts
@@ -1,11 +1,11 @@
 // For desktop
-export const phrases = ['Bruin Venture Labs'];
+export const phrases = ['VEST @UCLA'];
 export const paragraphPhrases = [
   "We're a community of entrepreneurial students that work together with VC firms & startups.",
 ];
 
 // For mobile
-export const mobilePhrases = ['Bruin Venture Labs'];
+export const mobilePhrases = ['VEST @UCLA'];
 export const mobileParagraphPhrases = [
   "We're a community of entrepreneurial students that work together with VC firms & startups.",
 ];
